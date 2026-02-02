9957a46b9c7382d1223214a6fd37064ceaf04264
Rapha <rapha@gamequestlog.local>
2026-02-02 20:29:54 +0100

    Feat: Add full-screen custom splash screen

--- src/main.js
+++ src/main.js
@@ -2,4 +2,17 @@ import { createApp } from 'vue'
 import './global.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
+
+// Remove splash screen
+const splash = document.getElementById('splash-screen')
+if (splash) {
+    setTimeout(() => {
+        splash.classList.add('loaded')
+        setTimeout(() => {
+            splash.remove()
+        }, 500)
+    }, 500) // Keep it visible for at least 500ms
+}
